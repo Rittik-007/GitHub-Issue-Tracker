05872c694ffe643cf1714e1ad9c9fbf8a26582e3
Rittik-007 <munshirittik03@gmail.com>
2026-03-31 00:45:42 +0600

    added login page and it's functionalities.

---
+++ js/log-in.js
@@ -0,0 +1,19 @@
+
+document.getElementById('btn-signIn').addEventListener("click", () => {
+    const userInput = document.getElementById('username-input');
+    const userValue = userInput.value;
+    // console.log(userValue);
+    const passwordInput = document.getElementById('password-input');
+    const passwordValue = passwordInput.value;
+    // console.log(passwordValue);
+
+    if(userValue == 'admin' && passwordValue == '1234'){
+        alert('Log in successful');
+
+        window.location.assign("/home.html");
+    }
+    else{
+        alert('Log in failed!');
+        return;
+    }
+})
