93af65b538727da9188d79cf121422631555d443
Rittik-007 <munshirittik03@gmail.com>
2026-03-30 13:58:11 +0600

    small changes, taking a break from coding.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const issueCountBox = document.getElementById('issue-count-box');
 const cardContainer = document.getElementById('card-container');
 
 
+// ======== buttons =========
 const toggleButton = (id) => {
     if (id == 'all-btn') {
         allButton.classList.remove('btn-active');
@@ -41,6 +42,7 @@ const toggleButton = (id) => {
 }
 
 
+// ======== Displaying all issues =========
 const loadAllIssue = () => {
     const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
 
@@ -67,8 +69,8 @@ const displayAllIssue = (issues) => {
                     <h3 class="font-semibold text-purple-950">${issue.title}</h3>
                     <p class="text-gray-400 font-light text-[0.8rem] mt-1.5 mb-1.5">${issue.description}</p>
                     <div class="flex gap-1.5 mt-2">
-                        <p class="bg-red-200 text-[0.7rem] text-red-500 py-0.5 px-3 rounded-2xl border border-red-500"><i class="fa-solid fa-bug"></i> ${issue.labels[0]}</p>
-                        <p class="bg-amber-100 text-[0.7rem] text-amber-500 py-0.5 px-3 rounded-2xl border border-amber-500"><i class="fa-solid fa-life-ring"></i> ${issue.labels[1]}</p>
+                        <p class="${issue.labels[0] == 'enhancement' ? 'bg-green-200 text-green-500 border-green-500' : issue.labels[0] == 'documentation' ? 'bg-gray-200 text-gray-500 border-gray-500' : 'bg-red-200 text-red-500 border-red-500'} text-[0.7rem] py-1 px-3 rounded-2xl border">${issue.labels[0] == 'bug' ? '<i class="fa-solid fa-bug"></i> ' : issue.labels[0] == 'documentation' ? '<i class="fa-regular fa-clipboard"></i> ' : '<i class="fa-solid fa-code"></i> '} ${issue.labels[0]}</p>
+                        <p class="${issue.labels[1] == 'enhancement' ? 'bg-green-200 text-green-500 border-green-500': issue.labels[1] == 'good first issue' ? 'bg-purple-200 text-purple-500 border-purple-500': issue.labels[1] == 'help wanted' ? 'bg-red-600 text-red-200 border-red-600' : 'bg-amber-100 text-amber-500 border-amber-500'} text-[0.7rem] py-1 px-3 rounded-2xl border"><i class="fa-solid fa-life-ring"></i> ${issue.labels[1]}</p>
                     </div>
                 </div>
                 <hr class="border-gray-300">
@@ -89,8 +91,8 @@ const displayAllIssue = (issues) => {
                     <h3 class="font-semibold text-purple-950">${issue.title}</h3>
                     <p class="text-gray-400 font-light text-[0.8rem] mt-1.5 mb-1.5">${issue.description}</p>
                     <div class="flex gap-1.5 mt-2">
-                        <p class="bg-red-200 text-[0.8rem] text-red-500 py-0.5 px-3 rounded-2xl border border-red-500"><i class="fa-solid fa-bug"></i> ${issue.labels[0]}</p>
-                        <p class="bg-amber-100 text-[0.8rem] text-amber-500 py-0.5 px-3 rounded-2xl border border-amber-500"><i class="fa-solid fa-life-ring"></i> ${issue.labels[1]}</p>
+                        <p class="${issue.labels[0] == 'enhancement' ? 'bg-green-200 text-green-500 border-green-500' : issue.labels[0] == 'documentation' ? 'bg-gray-200 text-gray-500 border-gray-500' : 'bg-red-200 text-red-500 border-red-500'} text-[0.7rem] py-1 px-3 rounded-2xl border">${issue.labels[0] == 'bug' ? '<i class="fa-solid fa-bug"></i> ' : issue.labels[0] == 'documentation' ? '<i class="fa-regular fa-clipboard"></i> ' : '<i class="fa-solid fa-code"></i> '} ${issue.labels[0]}</p>
+                        <p class="${issue.labels[1] == 'enhancement' ? 'bg-green-200 text-green-500 border-green-500': issue.labels[1] == 'good first issue' ? 'bg-purple-200 text-purple-500 border-purple-500': issue.labels[1] == 'help wanted' ? 'bg-red-600 text-red-200 border-red-600' : 'bg-amber-100 text-amber-500 border-amber-500'} text-[0.7rem] py-1 px-3 rounded-2xl border"><i class="fa-solid fa-life-ring"></i> ${issue.labels[1]}</p>
                     </div>
                 </div>
                 <hr class="border-gray-300">
@@ -136,8 +138,8 @@ const displayOpenIssue = (issues) => {
                     <h3 class="font-semibold text-purple-950">${issue.title}</h3>
                     <p class="text-gray-400 font-light text-[0.8rem] mt-1.5 mb-1.5">${issue.description}</p>
                     <div class="flex gap-1.5 mt-2">
-                        <p class="bg-red-200 text-[0.7rem] text-red-500 py-0.5 px-3 rounded-2xl border border-red-500"><i class="fa-solid fa-bug"></i> ${issue.labels[0]}</p>
-                        <p class="bg-amber-100 text-[0.7rem] text-amber-500 py-0.5 px-3 rounded-2xl border border-amber-500"><i class="fa-solid fa-life-ring"></i> ${issue.labels[1]}</p>
+                        <p class="${issue.labels[0] == 'enhancement' ? 'bg-green-200 text-green-500 border-green-500' : issue.labels[0] == 'documentation' ? 'bg-gray-200 text-gray-500 border-gray-500' : 'bg-red-200 text-red-500 border-red-500'} text-[0.7rem] py-1 px-3 rounded-2xl border">${issue.labels[0] == 'bug' ? '<i class="fa-solid fa-bug"></i> ' : issue.labels[0] == 'documentation' ? '<i class="fa-regular fa-clipboard"></i> ' : '<i class="fa-solid fa-code"></i> '} ${issue.labels[0]}</p>
+                        <p class="${issue.labels[1] == 'enhancement' ? 'bg-green-200 text-green-500 border-green-500': issue.labels[1] == 'good first issue' ? 'bg-purple-200 text-purple-500 border-purple-500': issue.labels[1] == 'help wanted' ? 'bg-red-600 text-red-200 border-red-600' : 'bg-amber-100 text-amber-500 border-amber-500'} text-[0.7rem] py-1 px-3 rounded-2xl border"><i class="fa-solid fa-life-ring"></i> ${issue.labels[1]}</p>
                     </div>
                 </div>
                 <hr class="border-gray-300">
@@ -182,8 +184,8 @@ const displayClosedIssue = (issues) => {
                     <h3 class="font-semibold text-purple-950">${issue.title}</h3>
                     <p class="text-gray-400 font-light text-[0.8rem] mt-1.5 mb-1.5">${issue.description}</p>
                     <div class="flex gap-1.5 mt-2">
-                        <p class="bg-red-200 text-[0.8rem] text-red-500 py-0.5 px-3 rounded-2xl border border-red-500"><i class="fa-solid fa-bug"></i> ${issue.labels[0]}</p>
-                        <p class="bg-amber-100 text-[0.8rem] text-amber-500 py-0.5 px-3 rounded-2xl border border-amber-500"><i class="fa-solid fa-life-ring"></i> ${issue.labels[1]}</p>
+                        <p class="${issue.labels[0] == 'enhancement' ? 'bg-green-200 text-green-500 border-green-500' : issue.labels[0] == 'documentation' ? 'bg-gray-200 text-gray-500 border-gray-500' : 'bg-red-200 text-red-500 border-red-500'} text-[0.7rem] py-1 px-3 rounded-2xl border">${issue.labels[0] == 'bug' ? '<i class="fa-solid fa-bug"></i> ' : issue.labels[0] == 'documentation' ? '<i class="fa-regular fa-clipboard"></i> ' : '<i class="fa-solid fa-code"></i> '} ${issue.labels[0]}</p>
+                        <p class="${issue.labels[1] == 'enhancement' ? 'bg-green-200 text-green-500 border-green-500': issue.labels[1] == 'good first issue' ? 'bg-purple-200 text-purple-500 border-purple-500': issue.labels[1] == 'help wanted' ? 'bg-red-600 text-red-200 border-red-600' : 'bg-amber-100 text-amber-500 border-amber-500'} text-[0.7rem] py-1 px-3 rounded-2xl border"><i class="fa-solid fa-life-ring"></i> ${issue.labels[1]}</p>
                     </div>
                 </div>
                 <hr class="border-gray-300">
